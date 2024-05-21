8b8cce242d782fcf81f6c78f45cc5d451dacb93d
Alexandru <ghalex@gmail.com>
2024-05-21 08:12:40 +0000

    small fixes

--- zp.config.js
+++ zp.config.js
@@ -1,15 +1,34 @@
-import { RetursAnalyzer } from 'zplang-backtest'
+import { analyzers } from 'zptrade-backtest'
 
 const config = {
   dataDir: "./data",
   apiUrl: "http://zapant.com/api",
+  inputs: {
+    assets: ["AAPL", "MSFT"],
+    initialCapital: 10000,
+    openPositions: [{
+      symbol: 'AAPL',
+      openDate: 1682366400000,
+      openPrice: 200.00,
+      closeDate: null,
+      closePrice: null,
+      units: 5,
+      side: 'long',
+      accountType: 'paper'
+    }]
+  },
   backtest: {
     date: "2024-05-01",
-    window: 5,
+    window: 10,
+    saveResult: "./example/result.json",
+    inputs: {
+      assets: []
+    },
     analyzers: [
-      new RetursAnalyzer()
+      // new analyzers.RetursAnalyzer()
+      new analyzers.PositionsAnalyzer()
     ]
   }
 }
 
-export default config
+export default config;
